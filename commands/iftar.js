97bccaa22375825fb0c030fd15cd559e43294104
quwicly91 <68401097+quwicly91@users.noreply.github.com>
2022-04-15 19:43:40 +0300

    Update iftar.js

--- commands/iftar.js
+++ commands/iftar.js
@@ -1,1 +1,78 @@
+const { MessageEmbed } = require("discord.js");
+const Discord = require('discord.js');
+const axios = require('axios');
 
+module.exports = {
+  name: "iftar",
+  description: "iftar zamanını gösterir",
+  usage: "",
+  permissions: {
+    channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
+    member: [],
+  },
+  aliases: ["iftar"],
+  /**
+   *
+   * @param {import("../structures/DiscordMusicBot")} client
+   * @param {import("discord.js").Message} message
+   * @param {string[]} args
+   * @param {*} param3
+   */
+  run: async (client, message, args, { GuildDB }) => {
+  const embed = new Discord.MessageEmbed()
+  const city = args[0];
+    if (!city) return message.channel.send('Şehir adı girmelisiniz.');
+    axios.get(`https://api.collectapi.com/pray/single?ezan=Ak%C5%9Fam&data.city=${city.toLowerCase()}`, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": "apikey 4Zr4x9ouWyXxQLCe4tqHij:6nH6cTfc0E4qfxrGRAjD6L"
+        }
+    }).then(res => {
+        const messageEmbed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setAuthor(`> ${city} şehri için iftar saati ${res.data.result[0].time}`,"https://media.giphy.com/media/1qb0OTg9ud1CRv60vS/giphy.gif" )
+        .setDescription(`\`\`\`İftar Vaktine Kalan Süre: ${res.data.result[0].hour} ${res.data.result[0].min}\`\`\``)
+        .setImage("https://www.gelisenbeyin.net/img/ramazan-oruc.gif")  
+        .setTimestamp()
+        .setFooter(client.user.username, client.user.avatarURL({ size:1024, dynamic:true, format:'png' }))
+    message.channel.send(messageEmbed);
+    }).catch(err => {
+        message.channel.send('Bir sorun ortaya çıktı. Komudu doğru kullandığınızdan emin olun.');
+        console.log(err);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
